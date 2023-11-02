6dcb51405f6b94842c111e3168d7a5459676c329
bhupinderjogi <149693110+bhupinderjogi@users.noreply.github.com>
2023-11-02 20:20:52 +0530

    Update DDAdottet.c

--- graphics/DDAdottet.c
+++ graphics/DDAdottet.c
@@ -1,51 +1,36 @@
- #include<stdio.h>
- #include <graphics.h>
- #include <stdlib.h>
- #include <stdio.h>
- int main(void)
- {
-   /* request auto detection */
-   int gdriver = DETECT, gmode;
-   int i,x,y,x1,y1,x2,y2,dx,dy,length,P0;
-   /* initialize graphics and local variables */
-   printf("Enter the starting coordinates of line");
-   scanf("%d%d",&x1,&y1);
-   printf("Enter the ending coordinates of line");
-   scanf("%d%d",&x2,&y2);
-   initgraph(&gdriver, &gmode, "");
-   dx=abs(x2-x1);
-   dy=abs(y2-y1);
-   if(dx>dy)
-    length=dx;
-   else
-    length=dy;
-   putpixel(x1,y1,2);
-   x=x1;
-   y=y1;
-   P0=2*dy-dx;
-   for(i=1;i<=length;i++)
-   {
-    if(P0<0)
-               {
-             if(i%2==0)
-            putpixel(x,y,0); 
-            x=x+1;
-            y=y+0;
-            putpixel(x,y,15);
-            P0=P0+2*dy;
-               }
-   else
-               {
-               if(i%2==0)
-            putpixel(x,y,0); 
-            x=x+1;
-            y=y+1;
-            putpixel(x,y,15);
-            P0=P0+2*dy-2*dx;
-               }
-    }
-   /* clean up */
-   getch();
-   closegraph();
-   return 0;
-}
+#include<graphics.h>   
+#include<stdio.h>   
+void main()   
+{   
+    int gd = DETECT ,gm, i;   
+    float x, y,dx,dy,steps;   
+    int x0, x1, y0, y1;   
+    initgraph(&gd, &gm, "C:\\TC\\BGI");   
+    setbkcolor(BLACK);   
+   x0 = 100 , y0 = 200, x1 = 500, y1 = 200;   
+    dx = (float)(x1 - x0);   
+    dy = (float)(y1 - y0);   
+    if(dx>=dy)   
+           {   
+        steps = dx;   
+    }   
+    else   
+           {   
+        steps = dy;   
+    }   
+    dx = dx/steps;   
+    dy = dy/steps;   
+    x = x0;   
+    y = y0;   
+    i = 1;   
+    while(i<= steps)  {   
+   if(i % 2==0) { 
+       putpixel(x, y, WHITE);   
+        x += dx;   
+        y += dy;   
+        i=i+1;   
+    } 
+ } 
+    getch();   
+    closegraph();   
+} 
